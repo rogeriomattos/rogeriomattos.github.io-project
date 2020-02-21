44262cbb97078fb7ecbac9e13005c262100790c6
rogerio.mattos7 <rogeriomattos38@outlook.com>
2020-02-21 08:47:19 -0300

    corrigindo layout

--- src/App.js
+++ src/App.js
@@ -1,22 +1,20 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          Rogerio Almeida de Mattos
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/rogeriomattos"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Veja meus projetos
         </a>
       </header>
     </div>
